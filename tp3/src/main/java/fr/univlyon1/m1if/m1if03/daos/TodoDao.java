2cf1a63bfc93a63ad9fe93827159dbe19ee09d67
Yanisse FERHAOUI <yanisse.ferhaoui@etu.univ-lyon1.fr>
2023-10-10 19:05:36 +0200

    edit todolist tododao

--- tp3/src/main/java/fr/univlyon1/m1if/m1if03/daos/TodoDao.java
+++ tp3/src/main/java/fr/univlyon1/m1if/m1if03/daos/TodoDao.java
@@ -2,10 +2,57 @@ package fr.univlyon1.m1if.m1if03.daos;
 
 import fr.univlyon1.m1if.m1if03.classes.Todo;
 
-public class TodoDao extends AbstractListDao<Todo> {
+import javax.naming.NameAlreadyBoundException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-    public TodoDao() {
+/**
+ * Classe TodoDao.
+ * */
+public class TodoDao extends AbstractMapDao<Todo> {
 
+    protected Serializable getKeyForElement(Todo element) {
+        return element.getTitle();
     }
+
+    public void createTodo(String title, String creator) throws NameAlreadyBoundException {
+        Todo todo = new Todo(title, creator);
+        this.add(todo);
+    }
+
+    public List<String> getIdTodosByOwner(String login) {
+        List<Todo> todos = new ArrayList<Todo>(this.findAll());
+        List<String> idTodos = new ArrayList<String>();
+        todos.forEach(todo -> {
+            if(todo.getAssignee().getLogin() == login) {
+                idTodos.add(todo.getAssignee().getLogin());
+            }
+        });
+        return idTodos;
+    }
+
+    public List<Todo> getTodosByOwner(String login) {
+        List<Todo> todos = new ArrayList<Todo>(this.findAll());
+        List<Todo> todosOwner = new ArrayList<Todo>();
+        todos.forEach(todo -> {
+            if(todo.getAssignee().getLogin() == login) {
+                todosOwner.add(todo);
+            }
+        });
+        return todosOwner;
+    }
+
+    public Todo getByTitle(String title) {
+        List<Todo> todos = new ArrayList<>(this.findAll());
+        for(int i = 0; i < todos.size(); i++) {
+            if(todos.get(i).getTitle().equals(title)) {
+
+            }
+        }
+    }
+
+
+
 
 }
--- tp3/src/main/java/fr/univlyon1/m1if/m1if03/servlets/TodoList.java
+++ tp3/src/main/java/fr/univlyon1/m1if/m1if03/servlets/TodoList.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 /**
