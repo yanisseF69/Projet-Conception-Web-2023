98d363f84d1ec5e78a48fbe4663beb771af81ccc
Yanisse FERHAOUI <yanisse.ferhaoui@etu.univ-lyon1.fr>
2023-10-17 18:22:12 +0200

    add TodoResourceController

---
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
@@ -0,0 +1,39 @@
+package fr.univlyon1.m1if.m1if03.controllers;
+
+import fr.univlyon1.m1if.m1if03.dao.TodoDao;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "TodoRessourceController", urlPatterns = {"/todos", "/todos/*"})
+public class TodoResourceController extends HttpServlet {
+
+    public void doGet() {
+
+    }
+
+    public void doPost() {
+
+    }
+
+    public void doPut() {
+
+    }
+
+    public void doDelete() {
+
+    }
+
+    private static class TodoRessource {
+        private final TodoDao todoDao;
+
+        TodoRessource(TodoDao todoDao) {
+            this.todoDao = todoDao;
+        }
+
+        public void create() {
+
+        }
+
+    }
+}
+
