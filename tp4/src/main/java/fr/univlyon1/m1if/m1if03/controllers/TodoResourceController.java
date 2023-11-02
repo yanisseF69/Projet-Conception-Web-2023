ed6fc5de543cd19e839311db329db4fd64ffc774
p2000998 <rayane.seghir@etu.univ-lyon1.fr>
2023-11-02 19:31:07 +0100

    test 1.4

--- tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/controllers/TodoResourceController.java
@@ -3,6 +3,7 @@ package fr.univlyon1.m1if.m1if03.controllers;
 import fr.univlyon1.m1if.m1if03.dao.TodoDao;
 import fr.univlyon1.m1if.m1if03.dto.todo.TodoDtoMapper;
 import fr.univlyon1.m1if.m1if03.dto.todo.TodoResponseDto;
+import fr.univlyon1.m1if.m1if03.exceptions.ForbiddenLoginException;
 import fr.univlyon1.m1if.m1if03.model.Todo;
 import fr.univlyon1.m1if.m1if03.model.User;
 import fr.univlyon1.m1if.m1if03.utils.UrlUtils;
@@ -14,6 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 
 import javax.naming.InvalidNameException;
+import javax.naming.NameAlreadyBoundException;
 import javax.naming.NameNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
@@ -102,8 +104,25 @@ public class TodoResourceController extends HttpServlet {
         }
     }
 
-    public void doPut() {
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] url = UrlUtils.getUrlParts(request);
+        String todo = url[1];
 
+        String title = request.getParameter("title");
+        String assignee = request.getParameter("login");
+
+        if (url.length == 2) {
+            try {
+                todoRessource.update(title, assignee);
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } catch (IllegalArgumentException ex) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            } catch (NameNotFoundException | InvalidNameException e) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 
     public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -111,8 +130,10 @@ public class TodoResourceController extends HttpServlet {
         String todoId = url[1];
         if (url.length == 2) {
             try {
+                todoRessource.delete(todoId);
+                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
 
-            } catch (IllegalArgumentException ex) {
+            } catch (IllegalArgumentException | NameNotFoundException | InvalidNameException ex) {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
             }
         } else {
@@ -127,8 +148,12 @@ public class TodoResourceController extends HttpServlet {
             this.todoDao = todoDao;
         }
 
-        public void create() {
-
+        public void create(@NotNull String title)
+                throws IllegalArgumentException, NameAlreadyBoundException, ForbiddenLoginException {
+            if (title == null || title.isEmpty()) {
+                throw new IllegalArgumentException("Le titre ne doit pas être null ou vide.");
+            }
+            todoDao.add(new Todo(title));
         }
 
         public Collection<Todo> readAll() {
@@ -140,6 +165,18 @@ public class TodoResourceController extends HttpServlet {
                 throw new IllegalArgumentException("Le login ne doit pas être null ou vide.");
             }
             return todoDao.findOne(login);
+        }
+
+        public void delete(@NotNull String login) throws IllegalArgumentException, NameNotFoundException, InvalidNameException {
+            if (login == null || login.isEmpty()) {
+                throw new IllegalArgumentException("Le login ne doit pas être null ou vide.");
+            }
+            todoDao.deleteById(login);
+        }
+
+        public void update(String title, String assignee) throws IllegalArgumentException, InvalidNameException, NameNotFoundException {
+            Todo todo = readOne("title");
+            todo.setAssignee(assignee);
         }
     }
 }
