d7cafd40cac1100990fe0a17e92ba1638e61d513
p2000998 <rayane.seghir@etu.univ-lyon1.fr>
2023-11-12 21:36:18 +0100

    cache

--- tp4/src/main/java/fr/univlyon1/m1if/m1if03/filters/UserCache.java
+++ tp4/src/main/java/fr/univlyon1/m1if/m1if03/filters/UserCache.java
@@ -1,6 +1,7 @@
 package fr.univlyon1.m1if.m1if03.filters;
 
 import fr.univlyon1.m1if.m1if03.dao.UserDao;
+import fr.univlyon1.m1if.m1if03.dto.user.UserDtoMapper;
 import fr.univlyon1.m1if.m1if03.model.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -13,6 +14,8 @@ import jakarta.servlet.ServletException;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * UserCache class.
@@ -20,10 +23,46 @@ import java.util.Collection;
 @WebFilter(filterName = "UserCache", urlPatterns = {"/users"})
 public class UserCache extends HttpFilter {
 
+    private Map<String, String> userETAGs = new HashMap<>();
+    private UserDao userDao;
+    private UserDtoMapper userMapper;
+
     public void init(FilterConfig config) throws ServletException {
         super.init(config);
     }
 
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        UserDao userDao = (UserDao) request.getServletContext().getAttribute("userDao");
+        String currentETAG = getTag(userDao.findAll());
+        response.addHeader("ETAG", "test");
+        response.setHeader("ETAG", "testt");
+
+        //requête GET
+        if (request.getMethod().equals("GET")) {
+            userDao = (UserDao) request.getAttribute("users");
+            userMapper = (UserDtoMapper) request.getAttribute("userMap");
+            String login = (String) request.getAttribute("login");
+            String userETAG = getETag(login);
+            String lastETAG = request.getHeader("If-None-Match");
+
+            if (lastETAG != null && currentETAG.equals(lastETAG)) {
+                response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+                return;
+            }
+            response.setHeader("ETag", currentETAG);
+            currentETAG.concat(login);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private String getETag(String login) {
+    }
+
+
+    /*
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
@@ -51,6 +90,7 @@ public class UserCache extends HttpFilter {
         }
         chain.doFilter(request, response);
     }
+     */
 
 
 
