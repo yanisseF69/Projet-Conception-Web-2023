720e83565a5c25377d5298f96584633d04e3c910
Yanisse FERHAOUI <yanisseferhaoui@gmail.com>
2023-11-14 14:45:02 +0100

    update api-base.js

--- tp5/src/main/webapp/js/api-base.js
+++ tp5/src/main/webapp/js/api-base.js
@@ -6,4 +6,4 @@
  * il faut rajouter l'origine du client dans les paramètres du filtre CORS (fichier <code>web.xml</code> du TP4).
  * @type {string}
  */
-const baseUrl = "http://localhost:8080/todos_war/";
+const baseUrl = "https://192.168.75.115/api/v3/";
